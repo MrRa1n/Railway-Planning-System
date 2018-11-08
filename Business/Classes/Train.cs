7cbed0fa0f0012abf12b69b2afeba448ace998cc
Cook <40316565@live.napier.ac.uk>
2018-11-08 14:46:51 +0000

    setup layers

---
+++ Business/Classes/Train.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Train
+    {
+        public String TrainID { get; set; }
+        public String[] Departure { get; set; }
+        public String[] Destination { get; set; }
+        public enum Type { Express, Stopping, Sleeper }
+        public String[] Intermediate { get; set; } // child classes
+        public DateTime DepartureTime { get; set; }
+        public DateTime DepartureDay { get; set; }
+        public bool SleeperCabin { get; set; }
+        public bool FirstClass { get; set; }
+    }
+}
---
+++ Business/TrainFacade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Business.Classes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    class TrainFacade
+    {
+        private StoppingTrain _stoppingTrain;
+        private ExpressTrain _expressTrain;
+        private SleeperTrain _sleeperTrain;
+
+        public TrainFacade()
+        {
+            _stoppingTrain = new StoppingTrain();
+            _expressTrain = new ExpressTrain();
+            _sleeperTrain = new SleeperTrain();
+        }
+    }
+}
